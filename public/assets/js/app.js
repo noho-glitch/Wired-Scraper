81150aabbe550b662b0d230d90c0fcb50a1ab5ab
Dave Nolan <dpn.nolan@gmail.com>
2019-12-15 23:07:51 -0500

    clear articles button working, scrape new articles requires 2 clicks

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,17 +1,44 @@
-$("#scrape").on("click", function(event) {
+$(document).ready(function() {
+  $("#scrape").on("click", function(event) {
     $.ajax("/scrape", {
       type: "GET"
     }).then(function() {
-      console.log("scraping");
-      location.reload();
+        console.log("scraping new articles")
+        // setTimeout(location.reload(), 1000);
+
     });
+    setTimeout(location.reload(), 1000);
   });
 
   $.getJSON("/articles", function(data) {
     // For each one
     for (var i = 0; i < data.length; i++) {
       // Display the apropos information on the page
-      $("#articles").append( "<a href='https://www.wired.com" + data[i].link + "'>" + "<div class='col-sm'>" + "<div class='card' style='width: 18rem;'>" + "<div class='card-body'>" + "<h5 class='card-title'>" + data[i].title + "</h5>" +"<p class='card-text'>" + data[i].link + "</p>" +
-      "</div>" + "</a>");
+      $("#articles").append(
+        "<a href='https://www.wired.com" +
+          data[i].link +
+          "'>" +
+          "<div class='col-sm'>" +
+          "<div class='card' style='width: 18rem;'>" +
+          "<div class='card-body'>" +
+          "<h5 class='card-title'>" +
+          data[i].title +
+          "</h5>" +
+          "<p class='card-text'>" +
+          data[i].link +
+          "</p>" +
+          "</div>" +
+          "</a>"
+      );
     }
   });
+
+  $("#clear").on("click", function(event) {
+    $.getJSON("/remove", {}).then(function() {
+      console.log("clearing");
+
+    });
+    location.reload();
+
+  });
+});
