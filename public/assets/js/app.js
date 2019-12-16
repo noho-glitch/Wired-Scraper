32e2a93385cea96b930b9da963216ee13342d350
Dave Nolan <dpn.nolan@gmail.com>
2019-12-15 22:45:56 -0500

    made each article a clickable card

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -6,3 +6,12 @@ $("#scrape").on("click", function(event) {
       location.reload();
     });
   });
+
+  $.getJSON("/articles", function(data) {
+    // For each one
+    for (var i = 0; i < data.length; i++) {
+      // Display the apropos information on the page
+      $("#articles").append( "<a href='https://www.wired.com" + data[i].link + "'>" + "<div class='col-sm'>" + "<div class='card' style='width: 18rem;'>" + "<div class='card-body'>" + "<h5 class='card-title'>" + data[i].title + "</h5>" +"<p class='card-text'>" + data[i].link + "</p>" +
+      "</div>" + "</a>");
+    }
+  });
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,60 +6,50 @@ var express = require("express");
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-
 // Route 1
 // =======
 // This route will retrieve all of the data
 
 module.exports = function(app) {
-    app.get("/articles", function(req, res) {
+  app.get("/articles", function(req, res) {
     db.Article.find({}, function(error, found) {
-        if (error) {
+      if (error) {
         console.log(error);
-        }
-        // Otherwise, send the result of this query to the browser
-        else {
+      }
+      // Otherwise, send the result of this query to the browser
+      else {
         res.json(found);
-        }
-    })
-    })
+      }
+    });
+  });
 
-    // Route 2
-    // =======
-    // When you visit this route, the server will
-    // scrape data from the site of your choice, and save it to
-    app.get("/scrape", function(req, res) {
+  app.get("/scrape", function(req, res) {
     axios.get("https://www.wired.com").then(function(response) {
-    var $ = cheerio.load(response.data);
+      var $ = cheerio.load(response.data);
 
-
-    // Select each element in the HTML body from which you want information.
-    $(".card-component__description").each(function(i, element) {
+      // Select each element in the HTML body from which you want information.
+      $(".card-component__description").each(function(i, element) {
         var results = {};
 
-
-        results.title = $(element).children().text();
-        results.link = $(element).find("a").attr("href");
+        results.title = $(element)
+          .children()
+          .text();
+        results.link = $(element)
+          .find("a")
+          .attr("href");
         // Save these results in an object that we'll push into the results array we defined earlier
-        db.Article.create(results)
-        .then(function(dbArticle) {
-            console.log(results)
-
-        })
+        db.Article.create(results).then(function(dbArticle) {
+          console.log(results);
+        });
+      });
     });
+  });
 
-    // Log the results once you've looped through each of the elements found with cheerio
-    // db.articles.save(results)
-    
+  app.get("/remove", function(req, res) {
+    res.send("data removed");
+
+    db.Article.remove({}, function(err) {
+      console.log("collection removed");
     });
-    })
-
-    app.get("/remove", function(req, res) {
-        res.send("data removed");
-      
-        db.Article.remove({}, function(err) { 
-            console.log('collection removed') 
-         });
-      })
+  });
 };
-
